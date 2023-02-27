8935d3dc06faada3013420672d8e12b411a73354
lowgain <logan.t2020@tutanota.com>
2023-02-27 16:42:36 -0500

    rafactor again

--- scraper.js
+++ scraper.js
@@ -9,28 +9,27 @@ const USER_AGENT = {
   }
 };
 
-fs.readFile("asins.txt", function(err, asinList) {
-  if (err) throw err; 
+fs.readFile("asins.txt", async (err, asinList) => {
   asinList = asinList.toString().split("\r\n");
-  asinList.pop();
-  fetchItems(asinList)
-    .then(items => console.table(items))
-    .catch(err => console.warn("There was an error, ", err));
+  let items = [];
+  for (asin of asinList) {
+    if ( asin != "" ) {
+      await getItem(asin)
+        .then(item => items.push(item))
+        .catch(err => console.warn("There was an error", err));
+    };
+  };
+  console.table(items);
+  if (err) throw err; 
 });
 
-async function fetchItems(asins) {
-  let items = [];
-  for (i in asins) {
-    const asin = asins[i];
-    const html = await axios.get(AMAZON_URL + asin, USER_AGENT)
-      .then(response => response.data)
-      .catch(err => console.warn("There was an error", err));
-    const $ = cheerio.load(html);
-    const name = $('span#productTitle').text().trim().slice(0, 80);
-    const sale = $('span.savingsPercentage').text();
-    let price = $('span.a-price-whole').text().split(".")[0];
-    const item = { asin, name, sale, price };
-    items.push(item);
-  };
-  return items;
+async function getItem(asin) {
+  const html = await axios.get(AMAZON_URL + asin, USER_AGENT)
+    .then(response => response.data)
+    .catch(err => console.warn("There was an error", err));
+  const $ = cheerio.load(html);
+  const name = $('span#productTitle').text().trim().slice(0, 80);
+  const sale = $('span.savingsPercentage').text();
+  const price = $('span.a-price-whole').text().split(".")[0];
+  return { asin, name, sale, price };
 };
